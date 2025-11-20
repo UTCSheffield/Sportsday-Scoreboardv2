4ff0b364b30a6ddba933ecd39c8a01fbd23cfede
Sean Outram <me@sean.cyou>
2025-11-20 13:03:38 +0000

    Fix scores not properly updating over websocket

--- src/js/controllers/score_update_controller.ts
+++ src/js/controllers/score_update_controller.ts
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     static override targets = ["output"];
+
     override connect() {
         document.addEventListener("wsmessage", (e: any) => {
             if (e.detail.channel != "scores") return;
